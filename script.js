1c527756f985f74efc28fc1a890567595e009645
RafaelM-218 <rafaeellmoreira218@gmail.com>
2025-12-02 12:50:06 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-class MobileNavBar {
-    constructor (mobileMenu, navLink, navList){
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLink = document.querySelectorAll(navLink);
-    }
-
-}
