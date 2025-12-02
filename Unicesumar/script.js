0c2ce2bffc7e85abefc5c0081e90ec6f51c223ea
RafaelM-218 <rafaeellmoreira218@gmail.com>
2025-12-02 12:54:55 -0300

    Add files via upload

---
+++ Unicesumar/script.js
@@ -0,0 +1,8 @@
+class MobileNavBar {
+    constructor (mobileMenu, navLink, navList){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLink = document.querySelectorAll(navLink);
+    }
+
+}
